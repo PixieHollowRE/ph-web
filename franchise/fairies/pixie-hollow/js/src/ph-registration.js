4c4cd9c50b8cfc5f609cec8f8d7953a0e353d8e6
Jessi <49761691+BuzzyJessibee@users.noreply.github.com>
2026-02-20 00:45:18 -0800

    Add Missing Meadow Assets
    More? Did i do it this time lmao

--- franchise/fairies/pixie-hollow/js/src/ph-registration.js
+++ franchise/fairies/pixie-hollow/js/src/ph-registration.js
@@ -26,56 +26,53 @@ function exitMod() {
 
 // Embed SWF -----------------------------------------------
 fairies.API.listen('whoAmI', function () {
+	var regXML = 'registration.xml';
 
-	$.getScript("https://tredir.go.com/capmon/GetDE/?set=j&param=connection&param=metrocode&param=domain&param=countryisocode", function() {
-		var regXML = (countryisocode == "can") ? 'registrationAB.xml' : 'registration.xml';
+	// Registration Flash Vars ------------------------------
+	var variables = {
+		bootURL					: 'boot_main_app.xml',
+		locale					: 'en_US',
+		contentSwfUrl			: 'gui/ph_f_gui_reg_registration.swf',
+		stringTableUrl			: 'stringtables/en_US/global.json',
+		loaderSwfUrl			: 'fairies_loader.swf',
+		purchaseURL				: 'https://disney.go.com/fairies/pixie-hollow/membership/purchase.html',
+		regConfig				: regXML,
+		login					: PATH.API + '/AccountLoginRequest',
+		logout					: PATH.API + '/logout',
+		whoAmI					: PATH.API + '/WhoAmIRequest',
+		newsletterID			: 'WDIGFamilySites',
+		recoveryPasswordURL		: 'https://register.go.com/global/fairies/recoverPassword?affiliateName=Disney_Fairies&appRedirect=http%3A%2F%2Fpixiehollow.com',
+		recoveryUsernameURL		: 'https://register.go.com/global/fairies/recoverMemberNames?affiliateName=Disney_Fairies&appRedirect=http%3A%2F%2Fpixiehollow.com',
+		termsOfUseURL			: 'http://disney.go.com/corporate/privacy/terms.html?ppLink=pp_wdig',
+		privacyPolicyURL		: 'http://disney.go.com/corporate/privacy/pp_wdig.html',
+		DOBCookieTO				: '30',
+		linkColor				: '#ffffff',
+		trackingManagerConfig	: 'tracker.xml',
+		IPDetectPath			: '', // http://tredir.go.com/capmon/GetDE
+		width					: '717',
+		height					: '650'
+	};
+	//----------------------------------------------------------
 
-		// Registration Flash Vars ------------------------------
-		var variables = {
-			bootURL					: 'boot_main_app.xml',
-			locale					: 'en_US',
-			contentSwfUrl			: 'gui/ph_f_gui_reg_registration.swf',
-			stringTableUrl			: 'stringtables/en_US/global.json',
-			loaderSwfUrl			: 'fairies_loader.swf',
-			purchaseURL				: 'https://disney.go.com/fairies/pixie-hollow/membership/purchase.html',
-			regConfig				: regXML,
-			login					: PATH.API + '/AccountLoginRequest',
-			logout					: PATH.API + '/logout',
-			whoAmI					: PATH.API + '/WhoAmIRequest',
-			newsletterID			: 'WDIGFamilySites',
-			recoveryPasswordURL		: 'https://register.go.com/global/fairies/recoverPassword?affiliateName=Disney_Fairies&appRedirect=http%3A%2F%2Fpixiehollow.com',
-			recoveryUsernameURL		: 'https://register.go.com/global/fairies/recoverMemberNames?affiliateName=Disney_Fairies&appRedirect=http%3A%2F%2Fpixiehollow.com',
-			termsOfUseURL			: 'http://disney.go.com/corporate/privacy/terms.html?ppLink=pp_wdig',
-			privacyPolicyURL		: 'http://disney.go.com/corporate/privacy/pp_wdig.html',
-			DOBCookieTO				: '30',
-			linkColor				: '#ffffff',
-			trackingManagerConfig	: 'tracker.xml',
-			IPDetectPath			: '', // http://tredir.go.com/capmon/GetDE
-			width					: '717',
-			height					: '650'
-		};
-		//----------------------------------------------------------
+	// Parameters ----------------------------------------------
+	var parameters = {
+		wmode				: 'transparent',
+		quality				: 'high',
+		allowscriptaccess	: 'always',
+		base				: '.'
+	};
+	//----------------------------------------------------------
 
-		// Parameters ----------------------------------------------
-		var parameters = {
-			wmode				: 'transparent',
-			quality				: 'high',
-			allowscriptaccess	: 'always',
-			base				: '.'
-		};
-		//----------------------------------------------------------
+	// Attributes ----------------------------------------------
+	var attributes = {
+		id	: 'PixiesRegistration'
+	};
+	//----------------------------------------------------------
 
-		// Attributes ----------------------------------------------
-		var attributes = {
-			id	: 'PixiesRegistration'
-		};
-		//----------------------------------------------------------
-
-		if(fairies.API.isRampClosed()) {
-			$('#registration').html('<h3>'+ fairies.API.getRampMsg() +'</h3>');
-		} else {
-			swfobject.embedSWF(PATH.CDN + '/pixie-hollow/ramp/registration/DVC_OS.swf', 'registration', '717', '650', '9.0.124', '', variables, parameters, attributes);
-		}
-	});
+	if(fairies.API.isRampClosed()) {
+		$('#registration').html('<h3>'+ fairies.API.getRampMsg() +'</h3>');
+	} else {
+		swfobject.embedSWF(PATH.CDN + '/pixie-hollow/ramp/registration/DVC_OS.swf', 'registration', '717', '650', '9.0.124', '', variables, parameters, attributes);
+	}
 });
 //----------------------------------------------------------
